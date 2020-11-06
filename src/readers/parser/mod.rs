0372f11057d9d3e148172aaa9e57838191b4fb78
astherath <farceriv@gmail.com>
2020-11-06 13:35:38 -0500

    passing implementation of nested struct parsing

--- src/readers/parser/mod.rs
+++ src/readers/parser/mod.rs
@@ -18,7 +18,7 @@ fn read_file_data(filename: &String) -> String {
 /// Also performs minimal sanitization, but mainly acts as the first
 /// barrier of safety for bad data (TOML validation performed at this step).
 pub struct InputFileReader {
-    toml_data: TomlConfig,
+    toml_data: WebAPI,
 }
 
 impl InputFileReader {
@@ -29,7 +29,7 @@ impl InputFileReader {
     /// populate the struct with a `toml::de::Deserializer`.
     pub fn from_file(filename: &String) -> InputFileReader {
         let file_data = read_file_data(filename);
-        let toml_data = TomlConfig::parse_toml(&file_data);
+        let toml_data = WebAPI::parse_toml(&file_data);
         InputFileReader { toml_data }
     }
 
@@ -39,16 +39,50 @@ impl InputFileReader {
     }
 }
 
-/// Serves as a facade for all of the `toml` reading/parsing.
+/// Provides all of the top-level unpacking (deserialization) from the toml file.
+///
+/// The struct also serves as the 1:1 schema of the input toml file, as well as all of
+/// it's sub-structs and optionals.
+///
+/// Most of the important data is in the `groups` field, as those house
+/// the low-level implementation details for the actual `Endpoints`.
 #[derive(Deserialize, Debug)]
-struct TomlConfig {
+struct WebAPI {
     title: String,
     version: String,
+    groups: Vec<EndpointGroup>,
 }
-impl TomlConfig {
+
+/// Holds the data for grouped endpoints working with the same logic.
+/// Mainly exists to conform easily to TOML structure.
+#[derive(Deserialize, Debug)]
+struct EndpointGroup {
+    name: String,
+    endpoints: Vec<Endpoint>,
+}
+
+/// Single-endpoint struct. Holds info such as type, route, input data, etc.
+/// A single WebAPI is composed of many `Endpoint` instances.
+#[derive(Deserialize, Debug)]
+struct Endpoint {
+    route: String,
+    http_verb: HTTPVerbs,
+}
+
+/// Very basic (and probably bad) implementation of the few accepted HTTP Verbs.
+/// TODO: replace this with a `std` lib enum of http verbs.
+#[derive(Deserialize, Debug)]
+enum HTTPVerbs {
+    Get,
+    Post,
+    Delete,
+    Update,
+}
+
+impl WebAPI {
     /// Top-level function to return the data within the config struct.
-    fn parse_toml(toml_str: &String) -> TomlConfig {
-        let parsed_toml: TomlConfig = toml::from_str(toml_str).unwrap();
+    fn parse_toml(toml_str: &String) -> WebAPI {
+        let parsed_toml: WebAPI = toml::from_str(toml_str).unwrap();
         parsed_toml
     }
 }
