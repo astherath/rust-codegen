65fdaa1ddebc3bd1dd167f9423daa04645ab7130
liam0215 <liam0215@gmail.com>
2020-11-13 16:32:21 -0500

    post_operator format rs WIP

--- src/writers/post_operator/mod.rs
+++ src/writers/post_operator/mod.rs
@@ -1,3 +1,4 @@
+<<<<<<< 4a5223bdd55eee667ff262e6d14613deea392d31
 use std::path::Path;
 use std::process::Command;
 use glob::glob;
@@ -11,4 +12,19 @@ fn format_all_rs(path: &Path) -> std::io::Result<()>{
         }
     }
     Ok(())
+=======
+use std::path::Path;
+use std::fs;
+use std::process::Command;
+use glob::glob;
+
+fn format_all_rs(path: &Path) -> std::io::Result<()>{
+    for entry in glob("*.rs").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => Command::new("rustfmt").args(path.to_str()).output()?,
+            Err(e) => println!({"{:?}", e}),
+        }
+    }
+    Ok(())
+>>>>>>> post_operator format rs WIP
 }
