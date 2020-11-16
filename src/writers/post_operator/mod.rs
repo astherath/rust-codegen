875578abfa43ff7a67e2740b1f99958b34ef28d0
Liam Arzola <liam0215@gmail.com>
2020-11-16 11:50:22 -0500

    format rs

--- src/writers/post_operator/mod.rs
+++ src/writers/post_operator/mod.rs
@@ -1,4 +1,4 @@
-<<<<<<< 4a5223bdd55eee667ff262e6d14613deea392d31
+
 use std::path::Path;
 use std::process::Command;
 use glob::glob;
@@ -12,19 +12,4 @@ fn format_all_rs(path: &Path) -> std::io::Result<()>{
         }
     }
     Ok(())
-=======
-use std::path::Path;
-use std::process::Command;
-use glob::glob;
-
-fn format_all_rs(path: &Path) -> std::io::Result<()>{
-    // Calls rustfmt on all files ending in .rs within path directory and all subdirectories
-    let query = path.join("*.rs");
-    for entry in glob(query.to_str().unwrap()).expect("Failed to read glob pattern") {
-        if let Ok(path) = entry {
-            Command::new("rustfmt").args(path.to_str()).output()?;
-        }
-    }
-    Ok(())
->>>>>>> post_operator format rs WIP
 }
