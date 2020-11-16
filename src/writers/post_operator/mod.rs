569100792af76a52af08f737803748fd20a116fd
liam0215 <liam0215@gmail.com>
2020-11-16 12:44:31 -0500

    fixed post_operator

--- src/writers/post_operator/mod.rs
+++ src/writers/post_operator/mod.rs
@@ -1,14 +1,11 @@
+// use glob::glob;
 use std::path::Path;
 use std::process::Command;
-use glob::glob;
 
-fn format_all_rs(path: &Path) -> std::io::Result<()>{
+pub fn format_all_rs(path: &Path) -> std::io::Result<()> {
     // Calls rustfmt on all files ending in .rs within path directory and all subdirectories
-    let query = path.join("*.rs");
-    for entry in glob(query.to_str().unwrap()).expect("Failed to read glob pattern") {
-        if let Ok(path) = entry {
-            Command::new("rustfmt").args(path.to_str()).output()?;
-        }
-    }
+    Command::new("cargo")
+        .args(&["fmt", "--manifest-path", path.join("Cargo.toml").to_str().unwrap()])
+        .output()?;
     Ok(())
 }
