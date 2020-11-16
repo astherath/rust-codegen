14eb1de1384766f57dbe78717b229f8432fdf7d6
liam0215 <liam0215@gmail.com>
2020-11-16 12:59:03 -0500

    fixed path generation style

--- src/writers/post_operator/mod.rs
+++ src/writers/post_operator/mod.rs
@@ -2,8 +2,9 @@
 use std::path::Path;
 use std::process::Command;
 
-pub fn format_all_rs(path: &Path) -> std::io::Result<()> {
+pub fn format_all_rs(path_string: &String) -> std::io::Result<()> {
     // Calls rustfmt on all files ending in .rs within path directory and all subdirectories
+    let path = Path::new(path_string);
     Command::new("cargo")
         .args(&["fmt", "--manifest-path", path.join("Cargo.toml").to_str().unwrap()])
         .output()?;
